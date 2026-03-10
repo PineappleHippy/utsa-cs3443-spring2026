36ccdde31bce112d6b6ae208af951f11a8ee70b5
John <55373552+PineappleHippy@users.noreply.github.com>
2026-03-10 17:22:43 -0500

    Add Driver for testing and program functionality.

---
+++ assignments/a3_klh551/src/Driver.java
@@ -0,0 +1,36 @@
+import application.letter.FormLetterBatch;
+import java.util.Scanner;
+
+public class Driver {
+
+   // Set to true for quick testing, false for interactive mode
+   private static final boolean TESTING = false;
+
+   public static void main(String[] args) throws Exception {
+      FormLetterBatch batch = new FormLetterBatch();
+
+      // Internal test program
+      if (TESTING) {
+         batch.generateAll("template.txt", "input", "output");
+      }
+
+      // User interface
+      else {
+         Scanner scanner = new Scanner(System.in);
+
+         System.out.print("Enter template file name: ");
+         String templatePath = scanner.nextLine();
+
+         System.out.print("Enter input directory name: ");
+         String inDirName = scanner.nextLine();
+
+         System.out.print("Enter output directory name: ");
+         String outDirName = scanner.nextLine();
+
+         scanner.close();
+
+         batch.generateAll(templatePath, inDirName, outDirName);
+         System.out.println("Alrighty! Letters written to: " + outDirName);
+      }
+   }
+}
