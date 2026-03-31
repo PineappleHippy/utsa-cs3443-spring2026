d9b282a974c271d9df9b933fb615e073cfe66b75
John <55373552+PineappleHippy@users.noreply.github.com>
2026-03-30 22:37:11 -0500

    Add Assignment 4 skeleton - JavaFX TicTacToe setup

---
+++ assignments/a4_klh551/src/application/model/Position.java
@@ -0,0 +1,18 @@
+package application.model;
+
+public class Position {
+   private int rowNum;
+   private int colNum;
+
+   public Position(int rowNum, int colNum){
+
+   }
+
+   public int getRowNum() {
+      return rowNum;
+   }
+
+   public int getColNum() {
+      return colNum;
+   }
+}
---
+++ assignments/a4_klh551/src/application/model/TictacToeBoard.java
@@ -0,0 +1,30 @@
+package application.model;
+
+import application.view.MainView;
+
+public class TictacToeBoard {
+   private Marker[][] board;
+   private MainView mainView;
+   private Marker startMarker;
+   private Marker currentMarker;
+
+   public void setMainView(MainView mainView){
+
+   }
+
+   public void putMarker(int rowNum, int colNum){
+
+   }
+
+   private void winningPositions (Marker marker){
+
+   }
+
+   public void startNewGame(){
+
+   }
+
+   private void clearBoard(){
+
+   }
+}
